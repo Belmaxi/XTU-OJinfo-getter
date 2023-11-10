8514d6615402ff7fc9497166091b22267a298096
Belmaxi <88465067+Belmaxi@users.noreply.github.com>
2023-11-10 17:26:23 +0800

    more readable

---
+++ export/xlsx_exporter.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from openpyxl import load_workbook
+from openpyxl.styles import Alignment
+
+class XlsxExport:
+
+    def __init__(self, _name = "Untitled.xlsx") -> None:
+        self.xlsx_name = _name
+
+    def export(self,info) -> None:
+        with pd.ExcelWriter(self.xlsx_name) as writer:
+            for i in info:
+                i[0] = pd.DataFrame(i[0])
+                i[0].to_excel(writer, sheet_name=i[1], index=False)
+
+    def set_style(self) -> None:
+        wb = load_workbook(self.xlsx_name)
+
+        for ws in wb._sheets:
+            ws.column_dimensions['C'].width = 20.0  # 调整列A宽
+            ws.column_dimensions['D'].width = 30.0
+
+        wb.save(self.xlsx_name)
--- main.py
+++ main.py
@@ -6,45 +6,36 @@ import os
 from utils.parser import Parser
 import requests
 from bs4 import BeautifulSoup
+from export.xlsx_exporter import XlsxExport
 import pandas as pd
-from openpyxl import load_workbook
-from openpyxl.styles import Alignment
 #自动安装依赖
-
-url = "https://acm.xtu.edu.cn/exam/index.php/solution/stat/cids/409,410,411,412,413,414,415,416,417,418,419,420,421,422,423,424,425,426,427,428,429,430"
-urls = [
-    [
-        f"https://acm.xtu.edu.cn/exam/index.php/solution/stat/order/{i[0]}/cids/409,410,411,412,413,414,415,416,417,418,419,420,421,422,423,424,425,426,427,428,429,430",
-        i[1]] for i in [[1, "班级"], [2, "专业"], [3, "学号"]]
-]
-
-
 check_pack()
 
-print("========正在爬取总表=======")
-parser = Parser(url)
-res = [["总数", pd.DataFrame(parser.get_all_standing())]]
-for i in urls:
-    print(f"========正在爬取{i[1]}顺序表=======")
-    parser = Parser(i[0])
-    res.append([i[1], pd.DataFrame(parser.get_all_standing())])
+exporter = XlsxExport("standing.xlsx")
+idx2tit = ["总表","班级","专业","学号"]
+
+urls = []
+res = []
+file = open("urls","r")
+for i in idx2tit:
+    try:
+        url = file.readline()
+    except Exception:
+        break
+    if len(url) > 4:
+        # urls.append([url,i])
+        parser = Parser(url)
+        print(f"========正在爬取{i}信息=======")
+
+        info = parser.get_all_standing()
+        res.append([info, i])
+
+file.close()
 
 print(f"======================")
 print("正在导出excel")
-input("press any to continue...")
-with pd.ExcelWriter('standing.xlsx') as writer:
-    for i in res:
-        i[1].to_excel(writer, sheet_name=i[0], index=False)
-
-wb = load_workbook('standing.xlsx')
-
-for ws in wb._sheets:
-    ws.column_dimensions['C'].width = 20.0  # 调整列A宽
-    ws.column_dimensions['D'].width = 30.0
-
-wb.save("standing.xlsx")
-
+exporter.export(res,)
+exporter.set_style()
 print("导出完成")
-input("press any to continue...")
 print("================================")
 input("press any to continue...")
